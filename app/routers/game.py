7524d351cc7c9e47a6981b3dd19682e301aee6f7
jandortiz <jefferson.garciaor@gmail.com>
2026-04-27 21:14:02 +0200

    style: organize spaces and delete prints

--- app/routers/game.py
+++ app/routers/game.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Request
+from app.services.game_service import game_service
 from app.schemas.game import NewGameResponse, GuessResponse, GameStatusResponse, GuessRequest
 from app.services.game_service import game_service
 
 
 router = APIRouter(prefix="/api/game")
 
+
 @router.post("/new", response_model=NewGameResponse)
 async def create_new_game(request: Request):
     # request.app.state.word_service
 
     word_data = await request.app.state.word_service.get_word_with_hint()
-    print(word_data)
 
     new_game_id = game_service.create_game(word=word_data['word'], hint=word_data['hint'])
     current_game_status = game_service.get_game_status(game_id=new_game_id)
@@ -28,7 +29,6 @@ def create_game_guess(game_id: str, guess: GuessRequest):
     correct = True if guess.letter.lower() in word else False
 
     current_game_status['correct'] = correct
-
     return current_game_status
 
 
